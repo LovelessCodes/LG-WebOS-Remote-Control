7c685c0dc729603d8291d66cdb39f9f0b99cca73
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-09-01 08:29:44 +0200

    feat: replace parking icon with volume-medium icon

--- src/lg-remote-control.ts
+++ src/lg-remote-control.ts
@@ -270,7 +270,7 @@ class LgRemoteControl extends LitElement {
                             <button class="btn ripple" style="border-radius: 50% 50% 0px 0px; margin: 0px auto 0px auto; height: 100%;" @click=${() => this._button("CHANNELUP")}><ha-icon icon="mdi:chevron-up"/></button>
                             <button class="btn" style="border-radius: 0px; cursor: default; margin: 0px auto 0px auto; height: 100%;"><ha-icon icon="${stateObj.attributes.is_volume_muted === true ? "mdi:volume-off" : "mdi:volume-high"}"/></button>
                             <button class="btn ripple" style="color:${stateObj.attributes.is_volume_muted === true ? "red" : ""}; height: 100%;" @click=${() => this._button("MUTE")}><span class="${stateObj.attributes.is_volume_muted === true ? "blink" : ""}"><ha-icon icon="mdi:volume-mute"></span></button>
-                            <button class="btn" style="border-radius: 0px; cursor: default; margin: 0px auto 0px auto; height: 100%;"><ha-icon icon="mdi:parking"/></button>
+                            <button class="btn" style="border-radius: 0px; cursor: default; margin: 0px auto 0px auto; height: 100%;"><ha-icon icon="mdi:volume-medium"/></button>
                             <button class="btn ripple" id="minusButton" style="border-radius: 0px 0px 50% 50%; margin: 0px auto 0px auto; height: 100%; touch-action: none;"
                                 @pointerdown=${(e: PointerEvent) => this._onVolumePointerDown("volume_down", e)}
                                 @pointerup=${(e: PointerEvent) => this._onVolumePointerUp(e)}
